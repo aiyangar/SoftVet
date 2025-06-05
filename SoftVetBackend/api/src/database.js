c3e30357cb4402b081d2fa4d7c581af271cc6e95
Gustavo Cárdenas <ing.gustavo.cardenas@gmail.com>
2025-06-05 12:33:06 -0600

    Refactor la configuración de la base de datos: mejora la legibilidad del código y carga automática de modelos

--- SoftVetBackend/api/src/database.js
+++ SoftVetBackend/api/src/database.js
@@ -1,23 +1,30 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
 
-const OwnersModel = require('./models/OwnersModel');
-const PetsModel = require('./models/PetsModel');
+const modelsDir = path.join(__dirname, 'models');
 
-const { 
-    DB_USER, 
-    DB_PASSWORD, 
+const {
+    DB_USER,
+    DB_PASSWORD,
     DB_HOST,
-    DB_PORT, 
-    DB_NAME } = process.env;
+    DB_PORT,
+    DB_NAME
+} = process.env;
 
 const sequelize = new Sequelize(
     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-    {logging: false}
-)
+    { logging: false }
+);
 
-OwnersModel(sequelize);
-PetsModel(sequelize);
+// Cargar todos los modelos automáticamente
+fs.readdirSync(modelsDir)
+    .filter(file => file.endsWith('.js'))
+    .forEach(file => {
+        const modelDefiner = require(path.join(modelsDir, file));
+        modelDefiner(sequelize);
+    });
 
 module.exports = {
     conn: sequelize
